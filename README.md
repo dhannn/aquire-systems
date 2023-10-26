d7c36baa8e0c0162cd281fecf35bc7aae8108018
Daniel Ramos <daniel_ramos@dlsu.edu.ph>
2023-10-26 13:02:21 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# AquiRe Systems
---
+++ src/App.js
@@ -0,0 +1,24 @@
+class App {
+    static port = 3000;
+
+    app = null;
+
+    constructor(express) {
+        this.app = express;
+    }
+
+    start() {
+
+    }
+
+    initializeViews() {
+
+    }
+
+    initializeUsers() {
+
+    }
+}
+
+const app = new App();
+app.start();
