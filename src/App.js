8afdfe5b3e8e774fcf937b53e2dc3e9cce336779
Zerithe <jonathandevera08@gmail.com>
2023-11-07 19:48:58 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,29 +5,37 @@ import { sequelize } from './DBConnection.js';
 import {User}  from'./schema/user.js';
 import {Enrolls} from './schema/enrolls.js'
 import {Student} from './schema/student.js'
-import { AdmissionRecord } from './schema/admissionrecord.js';
-import { Record } from './schema/record.js';
 //Relationship
 import './schema/relationship.js';
 //Routing
 import { AdminModel } from './user/admin/AdminModel.js';
 import { AdminContoller } from './user/admin/AdminController.js';
-//Dependencies
+import { PortalModel } from './user/portal/PortalModel.js';
+import { PortalContoller } from './user/portal/PortalController.js';
 import bodyParser from "body-parser";
 import  hbs from 'express-hbs';
+import cookieParser from 'cookie-parser';
+
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { GuidanceController } from './user/guidance/GuidanceController.js';
+import { GuidanceModel } from './user/guidance/guidanceModel.js';
 
 
 class App {
     static port = process.env.PORT || 3000;
     app = null;
+    userObjects = [];
     
     constructor(express) {
-        this.app = express;
-    
-        this.loadEnv();
+        this.app = express; 
+
         this.initializeViews();
+
+        this.portalModel = new PortalModel(); 
+        this.portalContoller = new PortalContoller();
+        this.portalContoller.bindModel(this.portalModel);
+        this.portalContoller.bindToApp(this.app);
     }
     
     start() {
@@ -38,12 +46,10 @@ class App {
             this.app
         });
     }
-
-    loadEnv() {
-    }
     
     async initializeViews() {
         this.app.use(bodyParser.urlencoded({extended: true}));
+        this.app.use(cookieParser());
         
         const __filename = fileURLToPath(import.meta.url);
         const __dirname = path.dirname(__filename);
@@ -51,6 +57,9 @@ class App {
         this.app.engine('hbs', hbs.express4({partialsDir: __dirname + '/views/partials'}));
         this.app.set('view engine', 'hbs');
         this.app.set('views', (__dirname) + '/views/layouts');
+
+        this.app.use(express.json());
+
         
         this.app.use(express.static(__dirname + "/public"));
     }
@@ -63,14 +72,16 @@ class App {
             console.error('Error syncing database:', error);
           }
     }
-    
+
     addUserModelController(model, controller) {
         controller.bindModel(model);
         controller.bindToApp(this.app);
+        this.portalContoller.addUserRoute(controller);
     }
 }
 
 const app = new App(express());
 
-app.addUserModelController(new AdminModel(), new AdminContoller())
+app.addUserModelController(new AdminModel(), new AdminContoller());
+app.addUserModelController(new GuidanceModel(), new GuidanceController());
 app.start();
