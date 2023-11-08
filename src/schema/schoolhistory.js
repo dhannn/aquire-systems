0021bf28816444bc8438a0a1aa7acaa2b3d123a0
Zerithe <jonathandevera08@gmail.com>
2023-11-08 13:26:52 +0800

    Create schoolhistory.js

---
+++ src/schema/schoolhistory.js
@@ -0,0 +1,28 @@
+import { Sequelize, DataTypes } from 'sequelize';
+import {sequelize} from '../DBConnection.js';
+import { Student } from './student.js';
+import { Enrolls } from './enrolls.js';
+
+export const SchoolHistory = sequelize.define('schoolhistory', {
+    studentId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+            model: Student,
+            key: 'studentId', 
+        }
+    },
+    enteredFrom: {
+        type: DataTypes.STRING,
+    },
+    gradeLevelEntered: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+            model: Enrolls,
+            key: 'schoolYear',
+        }
+    }
+})
