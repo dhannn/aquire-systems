47a85cb58f1a6fad407416c00748861c6ac5f2cd
Zerithe <jonathandevera08@gmail.com>
2023-11-11 13:56:18 +0800

    Update guidanceModel.js

--- src/user/guidance/guidanceModel.js
+++ src/user/guidance/guidanceModel.js
@@ -1,6 +1,7 @@
-import { Record } from "../../schema/record";
-import { AdmissionRecord } from "../../schema/admissionrecord";
-import { Enrolls } from "../../schema/enrolls";
+import { AdmissionRecord } from "../../schema/admissionrecord.js";
+import { Record } from "../../schema/record.js";
+import { Enrolls } from "../../schema/enrolls.js";
+import { SchoolHistory } from "../../schema/schoolhistory.js";
 
 export class GuidanceModel {
     
@@ -9,7 +10,7 @@ export class GuidanceModel {
      */
     static initializeRecordTypes(){
         async function initializeTypes() {
-            record = [
+            const record = [
                 {recordId:'MC', recordName:'Medical Certificate'},
                 {recordId:'RF', recordName:'Recommendation Form'},
                 {recordId:'SRF', recordName:'Scholastic Record Form'},
@@ -37,39 +38,54 @@ export class GuidanceModel {
     static addStudentRecord(id, recordType){
         async function addStudentRecord() {
             try{
-                Enrolls.findOne({
-                    where: {
-                        student_id: id
-                    },
-                    include: [
-                        {
-                            model: Enrolls,
-                            as: 'schoolYear',
-                            attributes:['schoolYear']
-                        }
-                    ]
-                }).then(record => {
-                    if(record) {
-                        const schoolYear = record.schoolYear.schoolYear;
-                        console.log('School Year found', schoolYear);
-                    } else {
-                        console.log('School Year not found');
-                    }
-                }).catch(error => {
-                    console.error('Error retrieving School Year', error);
+                const year = await Enrolls.findOne({
+                    where: {student_id: id},
+                    attributes: ['schoolYear']
                 });
-                const newRecords = recordType.map(type => ({
+                const existingRecords = await AdmissionRecord.findAll({
+                    where: {student_id: id},
+                    attributes: ['recordId']
+                });
+                const existingRecordIds = existingRecords.map(record => record.recordId);
+                const uniqueRecordIds = recordType.filter(id => !existingRecordIds.includes(id));
+
+                const newRecords = uniqueRecordIds.map(type => ({
                     student_id: id,
-                    schoolYear: schoolYear,
+                    schoolYear: year.schoolYear.toString(),
                     recordId: type
                 }));
                 const record = await AdmissionRecord.bulkCreate(newRecords);
                 console.log('Record inserted successfully', record);
-                return record;
             } catch(error) {
                 console.error('Error inserting record', error);
             }
         }
         addStudentRecord();
+    }
+
+    /**
+     * Adds school history of a student
+     * @param {String} id 
+     * @param {String} enteredFrom 
+     * @param {String} gradeLevelEntered 
+     * @param {String} schoolYearAdmitted 
+     * @param {String} otherSchoolsAttended 
+     */
+    static addStudentSchoolHistory(id, enteredFrom, gradeLevelEntered, schoolYearAdmitted, otherSchoolsAttended) {
+        async function addStudentSchoolHistory() {
+            try{
+                const newStudentSchoolHistory = await SchoolHistory.create({
+                    student_id: id,
+                    enteredFrom: enteredFrom,
+                    gradeLevelEntered: gradeLevelEntered,
+                    schoolYearAdmitted: schoolYearAdmitted,
+                    otherSchoolsAttended: otherSchoolsAttended
+                })
+                console.log('Student school history successfully created', newStudentSchoolHistory);
+            } catch (error) {
+                console.error('Error creating student school history', error);
+            }
+        }
+        addStudentSchoolHistory();
     }
 }
