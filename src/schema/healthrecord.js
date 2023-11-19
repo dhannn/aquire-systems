ebacf0ea3da9392d14ed70f08b4f337daf63cb99
Zerithe <jonathandevera08@gmail.com>
2023-11-19 13:18:49 +0800

    Create healthrecord.js

---
+++ src/schema/healthrecord.js
@@ -0,0 +1,35 @@
+import { Sequelize, DataTypes } from 'sequelize';
+import {sequelize} from '../DBConnection.js';
+
+export const HealthRecord = sequelize.define('healthrecord', {
+    student_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+            model: 'Students',
+            key: 'student_id', 
+        }
+    },
+    schoolYear: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+    },
+    vision: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    height: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    weight: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    specialCondition: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }
+});
