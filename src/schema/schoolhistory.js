2a0b850cf9bcade3cbe993f8f1feea61a4b88101
RichterDelaCruz <angelorichter.delacruz@gmail.com>
2023-11-25 04:18:14 +0800

    added new files

---
+++ src/schema/schoolhistory.js
@@ -0,0 +1,32 @@
+import { Sequelize, DataTypes } from 'sequelize';
+import {sequelize} from '../DBConnection.js';
+import { Student } from './student.js';
+import { Enrolls } from './enrolls.js';
+
+export const SchoolHistory = sequelize.define('schoolhistory', {
+    student_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+        references: {
+            model: Student,
+            key: 'student_id', 
+        }
+    },
+    enteredFrom: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    gradeLevelEntered: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    schoolYearAdmitted: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    otherSchoolsAttended: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+})
--- src/user/guidance/GuidanceModel.js
+++ src/user/guidance/GuidanceModel.js
@@ -1,3 +1,95 @@
+import { AdmissionRecord } from "../../schema/admissionrecord.js";
+import { Record } from "../../schema/record.js";
+import { Enrolls } from "../../schema/enrolls.js";
+import { SchoolHistory } from "../../schema/schoolhistory.js";
+
 export class GuidanceModel {
     
+    /**
+     * Initializes the record types
+     */
+    static initializeRecordTypes(){
+        async function initializeTypes() {
+            const record = [
+                {recordId:'MC', recordName:'Medical Certificate'},
+                {recordId:'RF', recordName:'Recommendation Form'},
+                {recordId:'SRF', recordName:'Scholastic Record Form'},
+                {recordId:'BEF', recordName:'Basic Education Form'},
+                {recordId:'SHSF', recordName:'Senior High School Form'},
+                {recordId:'IS', recordName:'Information Sheet'},
+            ];
+
+            Record.bulkCreate(record, {
+                updateOnDuplicate: ['recordName']
+            }).then(() => {
+                console.log('Records have been successfully ');
+            }).catch(error => {
+                console.error('Error adding records:', error);
+            });
+        }
+        initializeTypes();
+    }
+
+    /**
+     * adds a record for a student.
+     * @param {String} id 
+     * @param {String} recordType 
+     */
+    static addStudentRecord(id, recordType){
+        async function addStudentRecord() {
+            try{
+                const year = await Enrolls.findOne({
+                    where: {student_id: id},
+                    attributes: ['schoolYear']
+                });
+                const existingRecords = await AdmissionRecord.findAll({
+                    where: {student_id: id},
+                    attributes: ['recordId']
+                });
+                const existingRecordIds = existingRecords.map(record => record.recordId);
+                const uniqueRecordIds = recordType.filter(id => !existingRecordIds.includes(id));
+                 if(uniqueRecordIds.length == 0) {
+                     throw new Error('No unique record types selected');
+                 }
+                const newRecords = uniqueRecordIds.map(type => ({
+                    student_id: id,
+                    schoolYear: year.schoolYear.toString(),
+                    recordId: type
+                }));
+                const record = await AdmissionRecord.bulkCreate(newRecords);
+                console.log('Record inserted successfully', record);
+                return {record: record};
+            } catch(error) {
+                console.log('Error in Model', error);
+                return {error: error};
+            }
+        }
+        return addStudentRecord();
+    }
+
+    /**
+     * Adds school history of a student
+     * @param {String} id 
+     * @param {String} enteredFrom 
+     * @param {String} gradeLevelEntered 
+     * @param {String} schoolYearAdmitted 
+     * @param {String} otherSchoolsAttended 
+     */
+    static addStudentSchoolHistory(id, enteredFrom, gradeLevelEntered, schoolYearAdmitted, otherSchoolsAttended) {
+        async function addStudentSchoolHistory() {
+            try{
+                const newStudentSchoolHistory = await SchoolHistory.create({
+                    student_id: id,
+                    enteredFrom: enteredFrom,
+                    gradeLevelEntered: gradeLevelEntered,
+                    schoolYearAdmitted: schoolYearAdmitted,
+                    otherSchoolsAttended: otherSchoolsAttended
+                })
+                console.log('Student school history successfully created', newStudentSchoolHistory);
+            } catch (error) {
+                console.error('Error creating student school history', error);
+            }
+        }
+        addStudentSchoolHistory();
+    }
 }
