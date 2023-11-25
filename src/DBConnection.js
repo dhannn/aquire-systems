c83752e542dbbb6c21d11ab79f5ccb9b060fa622
Marco Lustre <jose_marco_lustre@dlsu.edu.ph>
2023-11-25 19:03:54 +0800

    changed db connection back

--- src/DBConnection.js
+++ src/DBConnection.js
@@ -1,4 +1,5 @@
 
+
 import { config } from "dotenv";
 import { Sequelize }  from 'sequelize';
 
@@ -6,8 +7,7 @@ config();
 
 const { DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env;
 
-export const sequelize = new Sequelize('aquire', 'root', '12345',  
- {
+export const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
 });
