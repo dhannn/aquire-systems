ff00b0815f87934ce22cc4fff9219a575f3c958f
Marco Lustre <jose_marco_lustre@dlsu.edu.ph>
2023-11-25 18:27:08 +0800

    I just changed the Navbar to navbar on every partial

--- src/DBConnection.js
+++ src/DBConnection.js
@@ -6,7 +6,8 @@ config();
 
 const { DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env;
 
-export const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+export const sequelize = new Sequelize('aquire', 'root', '12345',  
+ {
   host: 'localhost',
   dialect: 'mysql',
 });
