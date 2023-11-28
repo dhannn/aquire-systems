f38435b6a124146c6767cf0298bf7df44a2b8d92
Zerithe <jonathandevera08@gmail.com>
2023-11-28 16:23:50 +0800

    added edit student information

--- src/user/admin/AdminController.js
+++ src/user/admin/AdminController.js
@@ -11,6 +11,7 @@ export class AdminContoller extends UserController {
   allowedUserType = "A";
 
   initializeRoutes() {
+    this.createRoute("GET", "", this.viewStudents);
     this.createRoute("GET", "/", this.viewStudents);
     this.createRoute("GET", "/students", this.viewStudents);
     this.createRoute("GET", "/users", this.viewUsers);
@@ -27,8 +28,6 @@ export class AdminContoller extends UserController {
    */
 
   async addStudent(req, res) {
-    
-
     const currentSchoolYear = await CurrentSchoolYear.findOne({
       order: [["createdAt", "DESC"]],
     });
@@ -42,6 +41,7 @@ export class AdminContoller extends UserController {
     const nextfromYear = parseInt(currentSchoolYear.fromYear) + 1;
     const nexttoYear = parseInt(currentSchoolYear.toYear) + 1;
     const nextschoolyear = `${nextfromYear}-${nexttoYear}`;
+    var message;
 
     const students = await Student.findAll({
       attributes: ["student_id", "firstName", "middleInitial", "lastName"],
@@ -58,15 +58,50 @@ export class AdminContoller extends UserController {
       raw: true,
     });
 
-    try {
-      await this.model.addStudent(
-        req.body.student_id,
-        req.body.firstName,
-        req.body.middleInitial,
-        req.body.lastName,
-        req.body.grade,
-        req.body.section
+    if(req.body.edit_student){
+      console.log('updating student info');
+      const updateStudent = await this.model.updateExistingStudentInfo(
+          req.body.student_id,
+          req.body.firstName,
+          req.body.middleInitial,
+          req.body.lastName,
+          req.body.grade,
+          req.body.section
       );
+      if(updateStudent.error){
+        console.error(updateStudent.error);
+        message = { content: updateStudent.error };
+      } else {
+        console.log("Student Updated");
+        message = { isSuccess: true, content: "Student updated successfully!" };
+      }
+    } else {
+      console.log('adding student info');
+      try {
+        await this.model.addStudent(
+          req.body.student_id,
+          req.body.firstName,
+          req.body.middleInitial,
+          req.body.lastName,
+          req.body.grade,
+          req.body.section
+        );
+        console.log("Student Added");
+        message = { isSuccess: true, content: "Student added successfully!" };
+      } catch (error) {
+        console.error(error.message);
+        message = { content: error.message };
+      }
+    }
+    try {
+      const currentSchoolYear = await CurrentSchoolYear.findOne({
+        order: [["createdAt", "DESC"]],
+      });
+  
+      if (!currentSchoolYear) {
+        throw new Error("Current school year is not set");
+      }
+      const schoolYear = `${currentSchoolYear.fromYear}-${currentSchoolYear.toYear}`;
       const students = await Student.findAll({
         attributes: ["student_id", "firstName", "middleInitial", "lastName"],
         include: [
@@ -81,10 +116,11 @@ export class AdminContoller extends UserController {
         ],
         raw: true,
       });
-      
-      console.log("Student Added");
+        console.log("Student Added");
+        
+      res.set({'Refresh': `3; url=/admin/students?grade=${req.body.grade}`});
       res.render("Admin_Student", {
-        message: { isSuccess: true, content: "Student added successfully!" },
+        message: message,
         students: students,
         schoolyear: schoolYear,
         nextschoolyear: nextschoolyear
@@ -92,7 +128,33 @@ export class AdminContoller extends UserController {
     } catch (error) {
       console.error(error.message);
       res.render("Admin_Student", {
-        message: { content: error.message },
+        message: { content: "Failed to fetch students: " + error.message },
+        students: [],
+      });
+    }
+  }
+  
+
+  async addUser(req, res) {
+    const result = await this.model.addUser(
+      req.body.userName,
+      req.body.userPassword,
+      req.body.userType
+    );
+
+    if (result.error) {
+      if (result.error.includes("duplicate key error")) {
+        res.render("Admin_User", {
+          message: { content: "Username already exists!" },
+        });
+      } else {
+        res.render("Admin_User", {
+          message: { content: "Username already exists!" },
+        });
+      }
+    } else {
+      res.render("Admin_User", {
+        message: { isSuccess: true, content: "User added successfully!" },
         students: students,
         schoolyear: schoolYear,
         nextschoolyear: nextschoolyear
@@ -100,12 +162,12 @@ export class AdminContoller extends UserController {
     }
   }
 
-  async viewStudents(_, res) {
+  async viewStudents(req, res) {
     const allowed = await UserController.verifyUserPermission(
       this.allowedUserType,
-      _
+      req
     );
-    const loggedIn = UserController.checkifloggedIn(_);
+    const loggedIn = UserController.checkifloggedIn(req);
 
     if (!loggedIn) {
       return res.redirect("/");
@@ -123,7 +185,13 @@ export class AdminContoller extends UserController {
     const nextfromYear = parseInt(currentSchoolYear.fromYear) + 1;
     const nexttoYear = parseInt(currentSchoolYear.toYear) + 1;
     const nextschoolyear = `${nextfromYear}-${nexttoYear}`;
-
+    
+    var gradefilter;
+    if (req.query.grade == null) {
+      return res.redirect('/admin/students?grade=Kinder');
+    } else {
+      gradefilter = req.query.grade;
+    }
 
     const students = await Student.findAll({
       attributes: ["student_id", "firstName", "middleInitial", "lastName"],
@@ -133,6 +201,7 @@ export class AdminContoller extends UserController {
           attributes: ["grade", "section"],
           where: {
             schoolYear: schoolYear,
+            grade: gradefilter,
           },
           required: true,
         },
@@ -148,16 +217,45 @@ export class AdminContoller extends UserController {
       if (!students || students.length === 0) {
         return res.render("Admin_Student", {
           students: [],
-          message: "No students found for the current school year.",
           schoolyear: schoolYear,
-          nextschoolyear: nextschoolyear
+          nextschoolyear: nextschoolyear,
+          
+          K: gradefilter === 'Kinder',
+          SK: gradefilter === 'Senior Kinder',
+          G1: gradefilter === 'Grade 1',
+          G2: gradefilter === 'Grade 2',
+          G3: gradefilter === 'Grade 3',
+          G4: gradefilter === 'Grade 4',
+          G5: gradefilter === 'Grade 5',
+          G6: gradefilter === 'Grade 6',
+          G7: gradefilter === 'Grade 7',
+          G8: gradefilter === 'Grade 8',
+          G9: gradefilter === 'Grade 9',
+          G10: gradefilter === 'Grade 10',
+          G11: gradefilter === 'Grade 11',
+          G12: gradefilter === 'Grade 12',
         });
       }
 
       res.render("Admin_Student", { 
         students: students,
         schoolyear: schoolYear,
-        nextschoolyear: nextschoolyear
+        nextschoolyear: nextschoolyear,
+        //for current proccing
+        K: gradefilter === 'Kinder',
+        SK: gradefilter === 'Senior Kinder',
+        G1: gradefilter === 'Grade 1',
+        G2: gradefilter === 'Grade 2',
+        G3: gradefilter === 'Grade 3',
+        G4: gradefilter === 'Grade 4',
+        G5: gradefilter === 'Grade 5',
+        G6: gradefilter === 'Grade 6',
+        G7: gradefilter === 'Grade 7',
+        G8: gradefilter === 'Grade 8',
+        G9: gradefilter === 'Grade 9',
+        G10: gradefilter === 'Grade 10',
+        G11: gradefilter === 'Grade 11',
+        G12: gradefilter === 'Grade 12',
        });
     } catch (error) {
       console.error("Error fetching students:", error);
@@ -215,11 +313,13 @@ export class AdminContoller extends UserController {
      */
 
    async startNewSchoolYear(req, res) {
+    res.set({'Refresh': '3; url=/admin/students/'});
+    
     try {
-        await this.model.startNewSchoolYear();
-
+        const updatedSchoolYear = await this.model.startNewSchoolYear();
         res.render('Admin_Student', {
             message: { isSuccess: true, content: 'New school year started successfully!' },
+            schoolyear: `${updatedSchoolYear.fromYear} - ${updatedSchoolYear.toYear}`,
         });
     } catch (error) {
         console.error(error.message);
@@ -227,7 +327,7 @@ export class AdminContoller extends UserController {
             message: { content: error.message },
         });
     }
-}
+  }
     async viewUsers(_, res) {
         const allowed = await UserController.verifyUserPermission(this.allowedUserType, _)
         const loggedIn = UserController.checkifloggedIn(_);
--- src/user/admin/AdminModel.js
+++ src/user/admin/AdminModel.js
@@ -34,17 +34,17 @@ export class AdminModel {
         return insertUser();
     }
 
-    // updateCurrentSchoolYear method
-    async updateCurrentSchoolYear(fromYear, toYear) {
-        try {
-            // Update the current school year range in the CurrentSchoolYear table
-            await CurrentSchoolYear.create({ fromYear, toYear }, { upsert: true });
-            return { fromYear, toYear };
-        } catch (error) {
-            console.error('Error updating the current school year:', error);
-            throw error;
-        }
-    }
+    // // updateCurrentSchoolYear method
+    // async updateCurrentSchoolYear(fromYear, toYear) {
+    //     try {
+    //         // Update the current school year range in the CurrentSchoolYear table
+    //         await CurrentSchoolYear.create({ fromYear, toYear }, { upsert: true });
+    //         return { fromYear, toYear };
+    //     } catch (error) {
+    //         console.error('Error updating the current school year:', error);
+    //         throw error;
+    //     }
+    // }
 
     /**
      * Inserts the user to the database
@@ -52,7 +52,7 @@ export class AdminModel {
     viewUsers() {}
 
   /**
-   * Inserts the student to the database
+   * Inserts the student tow the database
    * @param {string} id
    * @param {string} firstName
    * @param {string} middleName
@@ -60,65 +60,82 @@ export class AdminModel {
    * @param {int} grade
    * @param {string} section
    */
+
   addStudent(id, firstName, middleName, lastName, grade, section) {
     return sequelize.transaction(async (t) => {
+     
+        if (middleName.length > 2) {
+            throw new Error("Middle initial should be at most two characters");
+        }
 
-    
-      if (middleName.length > 2) {
-        throw new Error("Middle initial should be at most two characters");
-      } 
+        const currentSchoolYear = await CurrentSchoolYear.findOne({
+            order: [['createdAt', 'DESC']],
+        });
 
-      const currentSchoolYear = await CurrentSchoolYear.findOne({
-        order: [['createdAt', 'DESC']], 
-      });
+        if (!currentSchoolYear) {
+            throw new Error("Current school year is not set");
+        }
 
-      if (!currentSchoolYear) {
-        throw new Error("Current school year is not set");
-      }
+        const schoolYear = `${currentSchoolYear.fromYear}-${currentSchoolYear.toYear}`;
 
-      const schoolYear = `${currentSchoolYear.fromYear}-${currentSchoolYear.toYear}`;
+ 
+        const existingStudent = await Student.findOne({
+            where: { student_id: id },
+            transaction: t,
+        });
 
-      const existingStudent = await Enrolls.findOne({
-        where: {
-          student_id: id,
-          schoolYear: schoolYear,
-        },
-        transaction: t,
-      });
+        if (existingStudent) {
+            if (existingStudent.firstName === firstName &&
+                existingStudent.middleInitial === middleName &&
+                existingStudent.lastName === lastName) {
 
-      if (existingStudent) {
-        throw new Error(
-          "Student with the same ID already exists for the current school year"
-        );
-      } 
+                const alreadyEnrolled = await Enrolls.findOne({
+                    where: {
+                        student_id: id,
+                        schoolYear: schoolYear,
+                    },
+                    transaction: t,
+                });
 
-      try {
-        const newStudent = await Student.create(
-          {
-            student_id: id,
-            firstName: firstName,
-            middleInitial: middleName,
-            lastName: lastName,
-          },
-          { transaction: t }
-        );
+                if (alreadyEnrolled) {
+                    throw new Error("Student is already enrolled for the current school year");
+                }
 
-        await newStudent.createEnroll(
-          {
-            student_id: id,
-            schoolYear: schoolYear,
-            grade: grade,
-            section: section,
-          },
-          { transaction: t }
-        );
+          
+                await Enrolls.create({
+                    student_id: id,
+                    schoolYear: schoolYear,
+                    grade: grade,
+                    section: section,
+                }, { transaction: t });
+                console.log("Enrollment added for existing student");
+                return existingStudent;
+            } else {
+                throw new Error("Student details do not match with existing records");
+            }
+        }
 
-        console.log("Student and enrollment inserted successfully");
-        return newStudent;
-      } catch (error) {
-        console.error("Error inserting student and/or enrollment:", error);
-        throw error;
-      }
+        try {
+            const newStudent = await Student.create({
+                student_id: id,
+                firstName: firstName,
+                middleInitial: middleName,
+                lastName: lastName,
+            }, { transaction: t });
+
+            await newStudent.createEnroll({
+                student_id: id,
+                schoolYear: schoolYear,
+                grade: grade,
+                section: section,
+            }, { transaction: t });
+
+            console.log("Student and enrollment inserted successfully");
+            return newStudent;
+        } catch (error) {
+            console.error("Error inserting student and/or enrollment:", error);
+            throw error;
+        }
     });
   }
 
@@ -156,22 +173,53 @@ export class AdminModel {
  }
 
 
-  async updateCurrentSchoolYear(fromYear, toYear) {
-    try {
-        // Update the current school year range in the CurrentSchoolYear table
-        const [updatedRows] = await CurrentSchoolYear.update(
-            { fromYear, toYear },
-            { where: {} }
-        );
+ async updateCurrentSchoolYear(fromYear, toYear) {
+  try {
+      // Update the current school year range in the CurrentSchoolYear table
+      const [updatedRows] = await CurrentSchoolYear.update(
+          { fromYear, toYear },
+          { where: {} }
+      );
 
-        if (updatedRows === 0) {
-            throw new Error('Failed to update the current school year');
+      if (updatedRows === 0) {
+          throw new Error('Failed to update the current school year');
+      }
+
+      return { fromYear, toYear };
+  } catch (error) {
+      console.error('Error updating the current school year:', error);
+      throw error;
+  }
+}
+
+  async updateExistingStudentInfo(id, firstName, middleName, lastName, grade, section) {
+    try{
+      const currentSchoolYear = await CurrentSchoolYear.findOne({
+        order: [['createdAt', 'DESC']],
+    });
+    const schoolYear = `${currentSchoolYear.fromYear}-${currentSchoolYear.toYear}`;
+      const student = await Student.update({
+        firstName: firstName,
+        middleInitial: middleName,
+        lastName: lastName,
+      }, {
+        where: {
+          student_id: id
         }
-
-        return { fromYear, toYear };
-    } catch (error) {
-        console.error('Error updating the current school year:', error);
-        throw error;
+      });
+      const enrolls = await Enrolls.update({
+        schoolYear: schoolYear,
+        grade: grade,
+        section: section,
+      }, {
+        where:{
+          student_id: id
+        }
+      });
+      return {student: student, enrolls: enrolls};
+    } catch(error) {
+      console.error('Error updating student information')
+      return {error: error};
     }
   }
 }
